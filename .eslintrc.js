e48612a2dc3c11278014f86725070872fff5fcf0
eggmun98 <eggmun98@gmail.com>
2024-01-14 16:27:29 +0900

    [2024.01.14] Comment eslintrc 및 prettierrc 설명 주석 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,50 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {},
 };
+
+/*
+
+  'no-console': 'warn',
+  // 코드에서 console.log와 같은 문장을 허용하지 않음
+
+  'semi': ['error', 'always'],
+  // 세미콜론을 항상 사용하도록 강제함
+
+  'react/jsx-no-duplicate-props': ['error'],
+  // 중복된 props를 허용하지 않음
+
+  'react/jsx-uses-vars': 'error',
+  // JSX 안에서 사용되는 변수들은 반드시 정의되어 있어야 함
+
+  'no-unused-vars': 'warn',
+  // 미사용 변수에 대한 경고
+
+  'react-hooks/rules-of-hooks': 'error',
+  // 훅의 규칙을 강제함
+
+  'react-hooks/exhaustive-deps': 'warn',
+  // 훅의 의존성 배열에 빠진 값이 있는지 검사
+
+  'jsx-quotes': ['error', 'prefer-double'],
+  // JSX 파일에서 단일 인용부호(') 대신 이중 인용부호(") 사용
+
+  'no-useless-computed-key': 'warn',
+  // 객체 리터럴에서 계산된 속성을 강제하지 않음
+
+  'object-curly-spacing': ['error', 'always'],
+  // 배열과 객체 리터럴에서 괄호 안에 공백 사용을 강제함
+
+  'array-bracket-spacing': ['error', 'never'],
+  'arrow-parens': ['error', 'as-needed'],
+  // 화살표 함수 괄호 사용 방식
+
+  'max-classes-per-file': ['error', 1],
+  // 파일당 최대 클래스 개수를 한 개로 제한
+
+  'import/order': 'warn'
+  // import 순서를 강제
+
+
+  */
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
-module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
-};
+module.exports =
+  {
+    arrowParens:
+      'avoid', // 화살표 함수에서 매개변수를 감싸는 괄호 어떻게 처리 할지 => avoid는 가능할 때 괄호 생략
+    bracketSameLine: true, // jsx에서 마지막 '>'를 다음 줄로 내릴지 여부 결정
+    bracketSpacing: false, // 객체 리터럴에서 중괄호 사이에 공백을 넣을지 여부 결정
+    singleQuote: true, // 문자열을 작성할 때 단일 인용부호(')를 사용할지 여부 결정
+    trailingComma:
+      'all', // 여러 줄을 가진 객체나 배열, 함수 매개변수 목록의 끝에 쉼표를 추가할지 여부 결정 => all은 모든 곳에 쉼표를 추가
+    semi: true, // 각 문의 끝에 세미콜론을 사용할지 여부 결정
+    printWidth: 120, // 한 줄의 길이 // 보토 80~120 사이를 많이 사용 함
+  };
